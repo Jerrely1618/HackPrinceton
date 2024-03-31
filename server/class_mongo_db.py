6bc052cfac3924e0c79382b134671c8d9be07619
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-03-31 00:29:23 -0400

    Integrated with API

--- server/class_mongo_db.py
+++ server/class_mongo_db.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -17,6 +17,18 @@ class MongoHandler:
 
     def generate_random_userid(self, length=8):
         return "".join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+    def return_parameters(self):
+        # Query MongoDB to get the most recent record
+        most_recent_record = self.collection.find_one(
+            {}, sort=[("_id", pymongo.DESCENDING)]
+        )
+
+        if most_recent_record:
+            # Remove the 'objectI' field if it exists
+            most_recent_record.pop("_id", None)
+
+        return most_recent_record
 
     def insert_info(self, cashiq, balance, weekly_expenditures, name, big_spends, goal):
         userid = self.generate_random_userid()
