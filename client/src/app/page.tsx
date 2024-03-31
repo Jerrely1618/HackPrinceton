965fd57926f3e474697b6310b3ab6bb821204d76
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2024-03-30 21:42:53 -0400

    Made UI updates

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 import { useState } from "react";
 import { Button } from "@nextui-org/react";
 import Image from "next/image";
 import { Link } from "@nextui-org/react";
 import bg from "../../public/bg.svg";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+
 interface UserData {
   fullName: string;
   estimatedIncome: string;
@@ -15,31 +17,49 @@ export default function Home() {
   const [showForm, setShowForm] = useState(false);
   const [UserData, setUserData] = useState<UserData[]>([]);
   const handleTryNowClick = () => {
-    setShowForm(true); 
+    setShowForm(true);
   };
   const handleBackClick = () => {
-    setShowForm(false)
-  }
-  const handleSubmit = (event) => {
+    setShowForm(false);
+  };
+  const handleSubmit = (event: any) => {
     event.preventDefault();
     const formData: UserData = {
-      fullName: (event.currentTarget.elements.namedItem("fullName") as HTMLInputElement).value,
-      estimatedIncome: (event.currentTarget.elements.namedItem("estimatedIncome") as HTMLInputElement).value,
-      weeklyExpenditures: (event.currentTarget.elements.namedItem("weeklyExpenditures") as HTMLInputElement).value,
-      bigSpendings: (event.currentTarget.elements.namedItem("bigSpendings") as HTMLInputElement).value,
-      goals: (event.currentTarget.elements.namedItem("goals") as HTMLInputElement).value,
+      fullName: (
+        event.currentTarget.elements.namedItem("fullName") as HTMLInputElement
+      ).value,
+      estimatedIncome: (
+        event.currentTarget.elements.namedItem(
+          "estimatedIncome"
+        ) as HTMLInputElement
+      ).value,
+      weeklyExpenditures: (
+        event.currentTarget.elements.namedItem(
+          "weeklyExpenditures"
+        ) as HTMLInputElement
+      ).value,
+      bigSpendings: (
+        event.currentTarget.elements.namedItem(
+          "bigSpendings"
+        ) as HTMLInputElement
+      ).value,
+      goals: (
+        event.currentTarget.elements.namedItem("goals") as HTMLInputElement
+      ).value,
     };
 
     setUserData([formData]);
   };
   return (
     <main className="flex min-h-screen flex-row w-full items-center justify-center bg-black overflow-hidden">
-      <div className="flex flex-col flex-1 items-center justify-center w-full bg-transparent text-white gap-16 kufam-font">
+      <div className="flex flex-col flex-1 items-center justify-center w-full bg-transparent text-white gap-16">
         <span
           style={{
             fontSize: "80px",
+            fontFamily: "Kufam, sans-serif",
             fontWeight: "bold",
-            backgroundImage: "linear-gradient(to right, #6B35B0, #DB278D, #CE1A30)",
+            backgroundImage:
+              "linear-gradient(to right, #6B35B0, #DB278D, #CE1A30)",
             WebkitBackgroundClip: "text",
             WebkitTextFillColor: "transparent",
             backgroundClip: "text",
@@ -48,57 +68,176 @@ export default function Home() {
           WealthWise!
         </span>
         {showForm ? (
-          <form onSubmit={handleSubmit} className="text-left mt-0 flex flex-col items-center gap-4 bg-white bg-opacity-10 py-5 px-10 rounded-lg">
-  <div className="grid grid-cols-2 gap-4 items-center w-full">
-    <label htmlFor="fullName" className="text-xl text-right mr-4">Full Name</label>
-    <input id="fullName" type="text" className="px-5 py-1 bg-white bg-opacity-10 rounded-md" />
-    
-    <label htmlFor="estimatedIncome" className="text-xl text-right mr-4">Estimated Income</label>
-    <input id="estimatedIncome" type="text" className="px-5 py-1 bg-white bg-opacity-10 rounded-md" />
-    
-    <label htmlFor="weeklyExpenditures" className="text-xl text-right mr-4">Weekly Expenditures</label>
-    <input id="weeklyExpenditures" type="text" className="px-5 py-1 bg-white bg-opacity-10 rounded-md" />
-    
-    <label htmlFor="bigSpendings" className="text-xl text-right mr-4">Big Spendings</label>
-    <input id="bigSpendings" type="text" className="px-5 py-1 bg-white bg-opacity-10 rounded-md" />
-    
-    <label htmlFor="goals" className="text-xl text-right mr-4">Goals</label>
-    <input id="goals" type="text" className="px-5 py-1 bg-white bg-opacity-10 rounded-md" />
-  </div>
-  <div className="flex space-x-5">
-  <Button type="submit" variant="shadow" className="mt-4 p-3 rounded-xl text-black font-bold text-lg" style={{ backgroundColor: "#00FF94", }}>
-    Submit
-  </Button>
-  <Button onClick={handleBackClick} variant="shadow" className="mt-4 p-3 rounded-xl text-black font-bold text-lg" style={{ backgroundColor: "#00FF94", }}>
-    Back
-  </Button>
-  </div>
-</form>
+          <form
+            onSubmit={handleSubmit}
+            className=" flex flex-col items-center justify-center w-full  gap-4  p-4 rounded-lg"
+          >
+            <div className="flex flex-col gap-4 items-center justify-center w-full">
+              <div>
+                {" "}
+                <label
+                  htmlFor="fullName"
+                  className="text-xl text-right mr-4"
+                  style={{
+                    fontFamily: "Kufam, sans-serif",
+                    fontSize: 15,
+                  }}
+                >
+                  Full Name
+                </label>
+                <input
+                  id="fullName"
+                  type="text"
+                  className="px-5 py-1 bg-white bg-opacity-10 rounded-md"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="estimatedIncome"
+                  className="text-xl text-right mr-4"
+                  style={{
+                    fontFamily: "Kufam, sans-serif",
+                    fontSize: 15,
+                  }}
+                >
+                  Estimated Income
+                </label>
+                <input
+                  id="estimatedIncome"
+                  type="text"
+                  className="px-5 py-1 bg-white bg-opacity-10 rounded-md"
+                />
+              </div>
 
+              <div>
+                <label
+                  htmlFor="weeklyExpenditures"
+                  className="text-xl text-right mr-4"
+                  style={{
+                    fontFamily: "Kufam, sans-serif",
+                    fontSize: 15,
+                  }}
+                >
+                  Weekly Expenditures
+                </label>
+                <input
+                  id="weeklyExpenditures"
+                  type="text"
+                  className="px-5 py-1 bg-white bg-opacity-10 rounded-md"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="bigSpendings"
+                  className="text-xl text-right mr-4"
+                  style={{
+                    fontFamily: "Kufam, sans-serif",
+                    fontSize: 15,
+                  }}
+                >
+                  Big Spendings
+                </label>
+                <input
+                  id="bigSpendings"
+                  type="text"
+                  className="px-5 py-1 bg-white bg-opacity-10 rounded-md"
+                />
+              </div>
+
+              <div>
+                {" "}
+                <label
+                  htmlFor="goals"
+                  className="text-xl text-right mr-4"
+                  style={{
+                    fontFamily: "Kufam, sans-serif",
+                    fontSize: 15,
+                  }}
+                >
+                  Goals
+                </label>
+                <input
+                  id="goals"
+                  type="text"
+                  className="px-5 py-1 bg-white bg-opacity-10 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="flex space-x-5">
+              <Button
+                type="submit"
+                className="p-3 rounded-xl text-white border-2 border-green-500 font-bold text-lg"
+                style={{
+                  backgroundColor: "transparent",
+                  fontFamily: "Kufam, sans-serif",
+                  fontSize: 12,
+                }}
+              >
+                Submit
+              </Button>
+              <Button
+                onClick={handleBackClick}
+                className="p-3 rounded-xl text-white border-2 border-green-500 font-bold text-lg"
+                style={{
+                  backgroundColor: "transparent",
+                  fontFamily: "Kufam, sans-serif",
+                  fontSize: 12,
+                }}
+              >
+                Back
+              </Button>
+            </div>
+          </form>
         ) : (
           <div className="flex flex-col items-center text-center w-full">
-            <span className="text-[30px] font-bold kufam-font">
+            <span
+              className="text-[24px] font-bold kufam-font"
+              style={{
+                fontFamily: "Kufam, sans-serif",
+              }}
+            >
               Start spending the smart way
             </span>
-            <span className="mt-3 text-[15px]">
+            <span
+              className="mt-3 text-[13px]"
+              style={{
+                fontFamily: "Kufam, sans-serif",
+              }}
+            >
               Get your finances back in control with WealthWise!
             </span>
-            <div className="flex space-x-3 mt-4">
-              <Button onClick={handleTryNowClick} variant="shadow" className="p-7 rounded-xl text-black font-bold text-lg" style={{ backgroundColor: "#00FF94", }}>
+            <div className="flex flex-row gap-2 mt-4 items-center justify-center">
+              <Button
+                onClick={handleTryNowClick}
+                className="text-white border-2 border-green-600 font-bold"
+                style={{
+                  background: "none",
+                  fontFamily: "Kufam, sans-serif",
+                  fontSize: 13,
+                }}
+              >
                 Try it for free now!
+                <ArrowOutwardIcon
+                  className="mb-1"
+                  sx={{
+                    fill: "white",
+                  }}
+                />
               </Button>
-        <Button
-          href="/profile"
-          as={Link}
-          variant="shadow"
-          className="p-7 rounded-xl text-black font-bold text-lg"
-          style={{
-            backgroundColor: "#00FF94",
-          }}
-        >
-          Login
-        </Button>
-        </div>
+              <Button
+                href="/profile"
+                as={Link}
+                className="text-white border-2 border-green-600 font-bold"
+                style={{
+                  background: "none",
+                  fontFamily: "Kufam, sans-serif",
+                  fontSize: 13,
+                }}
+              >
+                Login
+              </Button>
+            </div>
           </div>
         )}
       </div>
